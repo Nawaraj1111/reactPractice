45c58e517b9559c07eccbba756ba667ccbb79ac8
Pro <luitel.nawaraj12300@gmail.com>
2026-02-15 21:41:34 +0545

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Home.css';
+
+
+
+export const Home = (props) => {
+    const [value, setValue] = React.useState(0);
+
+    const handleIncrement = () => {
+            setValue(value + 1);
+    }
+
+    const handleDecrement = () => {
+            setValue(value - 1);
+    }
+
+    const handleReset = () => {
+            setValue(0);
+    }
+    const setColor = {
+        color: value > 0 ? 'green' : value < 0 ? 'red' : 'black'
+        }       
+return (
+    <div className='home-list'>
+            <ul>
+                    <li style={setColor}>{props.name} - {value}</li>
+            </ul>
+         <button onClick={handleIncrement}>+</button>
+         <button onClick={handleDecrement}>-</button>
+         <button onClick={handleReset}>Reset</button>
+    </div>
+    )
+}
