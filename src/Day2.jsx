8aa448a2abd7d65ae01e72ba4b2435c6369e3297
Pro <luitel.nawaraj12300@gmail.com>
2026-02-16 14:08:53 +0545

    Adding list and login value

---
+++ src/Day2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Day2 = () => {
+  return (
+    <div>Day2</div>
+  )
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './Home.css';
 
 
 
 export const Home = (props) => {
     const [value, setValue] = React.useState(0);
+    const [islogin, setIsLogin] = React.useState(false);
 
     const handleIncrement = () => {
             setValue(value + 1);
@@ -19,8 +21,19 @@ export const Home = (props) => {
     }
     const setColor = {
         color: value > 0 ? 'green' : value < 0 ? 'red' : 'yellow'
-        }       
+} 
+const fruits = ["Nawaraj", "luitel", "Nepal", "Kathmandu", "Lalitpur"];
+useEffect(() => {
+        console.log('Value changed:', value);
+      }, [value]);      
 return (
+        <>
+      <div>
+        <h1>Welcome to Home Page</h1>
+         {/* <button onClick={() => setIsLogin(!islogin)}>{islogin ? 'Logout' : 'Login'}</button> */}
+         {value>0 && <p>You are logged in!</p>}
+      </div>
+      
     <div className='home-list'>
             <ul>
                     <li style={setColor}>{props.name} - {value}</li>
@@ -31,6 +44,13 @@ return (
          <div>
                 <p className='copyright'>copyright @{new Date().getFullYear()}</p>
          </div>
+         <div>
+
+        <ul>
+                {fruits.map((fruit,index)=><li className='fruit-item' key={index}>{fruit}</li>)}
+        </ul>
+         </div>
     </div>
+    </>
     )
 }
